e7e146166e35a4bffbc03f14425b218916f443e1
bilal <bilaltahacs@gmail.com>
2022-09-16 01:41:38 -0400

    Test NestJS Server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,9 @@ export class AppComponent {
     // this.http.get('https://brobot.live/api/auth/twitch/login', {withCredentials: true}).subscribe(res => {
     //   console.log("AUTH LOGIN RES", res)
     // })
-     fetch('https://brobot.live/api/auth/twitch/login', { credentials: 'include' }).then(res=> {
+     fetch('https://brobot.live/api/auth/twitch/login', { credentials: 'include', mode: 'cors', headers: {
+         'Content-Type': 'application/json',
+       }, redirect: 'follow' }).then(res=> {
        res.json().then(data => {
          console.log("DATA", data)
        })
